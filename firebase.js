55dc3984e8a103c9130b769495f4698fbd68fc4d
Alex Pereira Katopodis <aperei83@its.jnj.com>
2022-10-11 11:52:48 -0300

    implement next-auth, firebase, login, and router page home and checkout

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWafCvdXKF82IuPy-Qi3o69-g9WEd5HEM",
+  authDomain: "amz-2-y.firebaseapp.com",
+  projectId: "amz-2-y",
+  storageBucket: "amz-2-y.appspot.com",
+  messagingSenderId: "142326431789",
+  appId: "1:142326431789:web:8d252d56f59a654577b8a4"
+};
+
+const app = initializeApp(firebaseConfig);
