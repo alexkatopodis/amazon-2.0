e91d1ee62e18dadba047a7ebd35d2bfb6977193b
Alex Pereira Katopodis <aperei83@its.jnj.com>
2022-10-13 16:07:38 -0300

    create backend webhook to get api stripe, send data to firebase db

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWafCvdXKF82IuPy-Qi3o69-g9WEd5HEM",
-  authDomain: "amz-2-y.firebaseapp.com",
-  projectId: "amz-2-y",
-  storageBucket: "amz-2-y.appspot.com",
-  messagingSenderId: "142326431789",
-  appId: "1:142326431789:web:8d252d56f59a654577b8a4"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
---
+++ src/pages/api/create-checkout-session.js
@@ -0,0 +1,37 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async (req, res) => {
+  const { items, email } = req.body;
+
+  const transformedItems = items.map((item) => ({
+    price_data: {
+      currency: "usd",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        description: item.description,
+        images: [item.image],
+      },
+    },
+    quantity: 1,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    // shipping_rates: ["shr_1LsT58Jb4K8gD6fcjD1jyYSf"],
+    shipping_address_collection: {
+      allowed_countries: ["GB", "US", "CA"],
+    },
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email: email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
+
+  res.status(200).json({ id: session.id });
+
+};
